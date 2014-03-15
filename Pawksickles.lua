a18211a16702e82c0852a67ac95902ea8237172e
Pawkette <pawkette.heals@gmail.com>
2014-03-14 21:05:24 -0700

    pawksickles now uses LMP and LAM

--- Pawksickles.lua
+++ Pawksickles.lua
@@ -1,112 +1,29 @@
+
 local kName         = 'Pawksickles'
 local Pawksickles   = {}
 local EventMgr      = GetEventManager()
+local CBM           = CALLBACK_MANAGER
+local LMP           = LibStub( 'LibMediaProvider-1.0' )
+if ( not LMP ) then return end
 
-local NORMAL        = [[Pawksickles/fonts/dejavusans.ttf]]
-local BOLD          = [[Pawksickles/fonts/dejavusans-bold.ttf]]
-local BOLDITALIC    = [[Pawksickles/fonts/dejavusans-boldoblique.ttf]]
-local ITALIC        = [[Pawksickles/fonts/dejavusans-oblique.ttf]]
-local NUMBER        = [[Pawksickles/fonts/dejavusansmono.ttf]]
-local SERIF         = [[Pawksickles/fonts/dejavuserif.ttf]]
-
-local THIN          = 'soft-shadow-thin'
-local THICK         = 'soft-shadow-thick'
-local SHADOW        = 'shadow'
+LMP:Register( 'font', 'DejaVu Sans', [[Pawksickles/fonts/dejavusans.ttf]] )
+LMP:Register( 'font', 'DejaVu Sans Bold', [[Pawksickles/fonts/dejavusans-bold.ttf]] )
+LMP:Register( 'font', 'DejaVu Sans BoldOblique', [[Pawksickles/fonts/dejavusans-boldoblique.ttf]] )
+LMP:Register( 'font', 'DejaVu Sans Oblique', [[Pawksickles/fonts/dejavusans-oblique.ttf]] )
+LMP:Register( 'font', 'DejaVu Sans Mono', [[Pawksickles/fonts/dejavusansmono.ttf]] )
+LMP:Register( 'font', 'DejaVu Serif', [[Pawksickles/fonts/dejavuserif.ttf]] )
 
 function Pawksickles:OnLoaded( event, addon )
     if ( addon ~= kName ) then
         return 
     end
 
-    self:SetFont( ZoFontWinH1, BOLD, 22, THICK )
-    self:SetFont( ZoFontWinH2, BOLD, 20, THICK )
-    self:SetFont( ZoFontWinH3, BOLD, 18, THICK )
-    self:SetFont( ZoFontWinH4, BOLD, 16, THICK )
-    self:SetFont( ZoFontWinH5, BOLD, 14, THIN )
-
-    self:SetFont( ZoFontWinT1, NORMAL, 12, THIN )
-
-    self:SetFont( ZoFontGame, NORMAL, 12, THIN )
-    self:SetFont( ZoFontGameMedium, NORMAL, 12, THIN )
-    self:SetFont( ZoFontGameBold, BOLD, 12, THIN )
-    self:SetFont( ZoFontGameOutline, BOLD, 12, THIN )
-    self:SetFont( ZoFontGameShadow, BOLD, 12, THIN )
-
-    self:SetFont( ZoFontGameSmall, NORMAL, 9, THIN )
-    self:SetFont( ZoFontGameLarge, NORMAL, 14, THIN )
-    self:SetFont( ZoFontGameLargeBold, BOLD, 14, THICK )
-    self:SetFont( ZoFontGameLargeBoldShadow, BOLD, 14, THICK )
-
-    self:SetFont( ZoFontHeader, BOLD, 12, THIN )
-    self:SetFont( ZoFontHeader2, BOLD, 14, THIN )
-    self:SetFont( ZoFontHeader3, BOLD, 16, THIN )
-    self:SetFont( ZoFontHeader4, BOLD, 18, THIN )
-
-    self:SetFont( ZoFontCallout, BOLD, 20, THICK )
-    self:SetFont( ZoFontCallout2, BOLD, 22, THICK )
-    self:SetFont( ZoFontCallout3, BOLD, 24, THICK )
-
-    self:SetFont( ZoFontEdit, NORMAL, 12, SHADOW )
-    self:SetFont( ZoLargeFontEdit, NORMAL, 21, SHADOW )
-
-    self:SetFont( ZoFontChat, NORMAL, 12, THIN )
-    self:SetFont( ZoFontEditChat, NORMAL, 14, SHADOW )
-
-    self:SetFont( ZoFontWindowTitle, BOLDITALIC, 24, THICK )
-    self:SetFont( ZoFontWindowSubtitle, NORMAL, 14, THICK )
-
-    self:SetFont( ZoFontTooltipTitle, BOLDITALIC, 18 )
-    self:SetFont( ZoFontTooltipSubtitle, NORMAL, 14 )
-
-    self:SetFont( ZoFontAnnounce, BOLDITALIC, 22, THICK )
-    self:SetFont( ZoFontAnnounceMessage, BOLD, 20, THICK )
-
-    self:SetFont( ZoFontAnnounceSmall, BOLDITALIC, 12, THICK )
-    self:SetFont( ZoFontAnnounceMedium, BOLDITALIC, 20, THICK )
-    self:SetFont( ZoFontAnnounceLarge, BOLDITALIC, 28, THICK )
-
-    self:SetFont( ZoFontCenterScreenAnnounceLarge, BOLDITALIC, 28, THICK )
-    self:SetFont( ZoFontCenterScreenAnnounceSmall, BOLDITALIC, 20, THICK )
-
-    self:SetFont( ZoFontAlert, BOLDITALIC, 18, THICK )
-
-    self:SetFont( ZoFontConversationName, BOLDITALIC, 20, THICK )
-    self:SetFont( ZoFontConversationText, NORMAL, 18, THICK )
-    self:SetFont( ZoFontConversationOption, ITALIC, 14, THICK )
-    self:SetFont( ZoFontConversationQuestReward, NORMAL, 14, THICK )
-
-    self:SetFont( ZoFontKeybindStripKey, BOLD, 14, THIN )
-    self:SetFont( ZoFontKeybindStripDescription, BOLD, 14, THICK )
-
-    --self:SetFont( ZoCraftingInstruction, NORMAL, 30, THICK )
-    self:SetFont( ZoInteractionPrompt, NORMAL, 16, THIN )
-
-    self:SetFont( ZoFontBookPaper, SERIF, 14 )
-    self:SetFont( ZoFontBookSkin, SERIF, 14 )
-    self:SetFont( ZoFontBookRubbing, SERIF, 14 )
-    self:SetFont( ZoFontBookLetter, SERIF, 14 )
-    self:SetFont( ZoFontBookNote, SERIF, 14 )
-    self:SetFont( ZoFontBookScroll, SERIF, 14 )
-    self:SetFont( ZoFontBookTablet, SERIF, 14 )
-    self:SetFont( ZoFontBookPaperTitle, SERIF, 20 )
-    self:SetFont( ZoFontBookSkinTitle, SERIF, 20 )
-    self:SetFont( ZoFontBookRubbingTitle, SERIF, 20 )
-    self:SetFont( ZoFontBookLetterTitle, SERIF, 20 )
-    self:SetFont( ZoFontBookNoteTitle, SERIF, 20 )
-    self:SetFont( ZoFontBookScrollTitle, SERIF, 20 )
-    self:SetFont( ZoFontBookTabletTitle, SERIF, 20 )
-        
-    self:SetFont( ZoFontBossName, BOLDITALIC, 16, THIN )
-    self:SetFont( ZoFontBoss, BOLD, 12, THIN )
+    CBM:RegisterCallback( 'PAWKSICKLES_FONT_CHANGED', function( ... ) self:SetFont( ... ) end )
+    CBM:FireCallbacks( 'PAWKSICKLES_LOADED' )
 end
 
-function Pawksickles:SetFont( object, fontFace, size, decoration )
-    local fmt = '%s|%d'
-    if ( decoration ) then
-        fmt = fmt .. '|%s'
-    end
-
-    object:SetFont( string.format( fmt, fontFace, size, decoration ) )
+function Pawksickles:SetFont( object, font )
+    _G[ object ]:SetFont( font )
 end
 
 EventMgr:RegisterForEvent( 'Pawksickles', EVENT_ADD_ON_LOADED, function( ... ) Pawksickles:OnLoaded( ... ) end )
---
+++ PawksicklesConfig.lua
@@ -0,0 +1,188 @@
+
+local LAM = LibStub( 'LibAddonMenu-1.0' )
+local LMP = LibStub( 'LibMediaProvider-1.0' )
+
+if ( not LAM ) then return end
+if ( not LMP ) then return end
+
+local tsort = table.sort
+local tinsert = table.insert
+
+local PawksicklesConfig = 
+{
+    _name = '_pawksickles',
+    _headers = setmetatable( {}, { __mode = 'kv' } )
+}
+
+local CBM = CALLBACK_MANAGER
+
+local THIN          = 'soft-shadow-thin'
+local THICK         = 'soft-shadow-thick'
+local SHADOW        = 'shadow'
+local NONE          = 'none'
+ 
+local defaults =
+{
+    ZoFontWinH1 = { face = 'DejaVu Sans Bold', size = 22, decoration = THICK },
+    ZoFontWinH2 = { face = 'DejaVu Sans Bold', size = 20, decoration = THICK },
+    ZoFontWinH3 = { face = 'DejaVu Sans Bold', size = 18, decoration = THICK },
+    ZoFontWinH4 = { face = 'DejaVu Sans Bold', size = 16, decoration = THICK },
+    ZoFontWinH5 = { face = 'DejaVu Sans Bold', size = 14, decoration = THIN },
+
+    ZoFontWinT1 = { face = 'DejaVu Sans', size = 12, decoration = THIN },
+
+    ZoFontGame = { face = 'DejaVu Sans', size = 12, decoration = THIN },
+    ZoFontGameMedium = { face = 'DejaVu Sans', size = 12, decoration = THIN },
+    ZoFontGameBold = { face = 'DejaVu Sans Bold', size = 12, decoration = THIN },
+    ZoFontGameOutline = { face = 'DejaVu Sans Bold', size = 12, decoration = THIN },
+    ZoFontGameShadow = { face = 'DejaVu Sans Bold', size = 12, decoration = THIN },
+    
+    ZoFontGameSmall = { face = 'DejaVu Sans', size = 9, decoration = THIN },
+    ZoFontGameLarge = { face = 'DejaVu Sans', size = 14, decoration = THIN },
+    ZoFontGameLargeBold = { face = 'DejaVu Sans Bold', size = 14, decoration = THICK },
+    ZoFontGameLargeBoldShadow = { face = 'DejaVu Sans Bold', size = 14, decoration = THICK },
+
+    ZoFontHeader  = { face = 'DejaVu Sans Bold', size = 12, decoration = THIN },
+    ZoFontHeader2  = { face = 'DejaVu Sans Bold', size = 14, decoration = THIN },
+    ZoFontHeader3  = { face = 'DejaVu Sans Bold', size = 16, decoration = THIN },
+    ZoFontHeader4  = { face = 'DejaVu Sans Bold', size = 18, decoration = THIN },
+
+    ZoFontCallout  = { face = 'DejaVu Sans Bold', size = 20, decoration = THICK },
+    ZoFontCallout2  = { face = 'DejaVu Sans Bold', size = 22, decoration = THICK },
+    ZoFontCallout3  = { face = 'DejaVu Sans Bold', size = 24, decoration = THICK },
+
+    ZoFontEdit  = { face = 'DejaVu Sans', size = 12, decoration = SHADOW },
+    ZoLargeFontEdit  = { face = 'DejaVu Sans', size = 21, decoration = SHADOW },
+    
+    ZoFontChat  = { face = 'DejaVu Sans', size = 12, decoration = THIN },
+    ZoFontEditChat  = { face = 'DejaVu Sans', size = 14, decoration = SHADOW },
+    
+    ZoFontWindowTitle  = { face = 'DejaVu Sans BoldOblique', size = 24, decoration = THICK },
+    ZoFontWindowSubtitle  = { face = 'DejaVu Sans', size = 14, decoration = THICK },
+
+    ZoFontTooltipTitle  = { face = 'DejaVu Sans BoldOblique', size = 18, decoration = NONE },
+    ZoFontTooltipSubtitle  = { face = 'DejaVu Sans', size = 14, decoration = NONE },
+
+    ZoFontAnnounce  = { face = 'DejaVu Sans BoldOblique', size = 22, decoration = THICK },
+    ZoFontAnnounceMessage  = { face = 'DejaVu Sans Bold', size = 20, decoration = THICK },
+
+    ZoFontAnnounceSmall  = { face = 'DejaVu Sans BoldOblique', size = 12, decoration = THICK },
+    ZoFontAnnounceMedium  = { face = 'DejaVu Sans BoldOblique', size = 20, decoration = THICK },
+    ZoFontAnnounceLarge  = { face = 'DejaVu Sans BoldOblique', size = 28, decoration = THICK },
+
+    ZoFontCenterScreenAnnounceLarge  = { face = 'DejaVu Sans BoldOblique', size = 28, decoration = THICK },
+    ZoFontCenterScreenAnnounceSmall  = { face = 'DejaVu Sans BoldOblique', size = 20, decoration = THICK },
+
+    ZoFontAlert  = { face = 'DejaVu Sans BoldOblique', size = 18, decoration = THICK },
+
+    ZoFontConversationName  = { face = 'DejaVu Sans BoldOblique', size = 20, decoration = THICK },
+    ZoFontConversationText  = { face = 'DejaVu Sans', size = 18, decoration = THICK },
+    ZoFontConversationOption  = { face = 'DejaVu Sans Oblique', size = 14, decoration = THICK },
+    ZoFontConversationQuestReward  = { face = 'DejaVu Sans', size = 14, decoration = THICK },
+
+    ZoFontKeybindStripKey  = { face = 'DejaVu Sans Bold', size = 14, decoration = THIN },
+    ZoFontKeybindStripDescription  = { face = 'DejaVu Sans Bold', size = 14, decoration = THICK },
+    
+    ZoInteractionPrompt  = { face = 'DejaVu Sans', size = 16, decoration = THIN },
+    
+    ZoFontBookPaper  = { face = 'DejaVu Serif', size = 14, decoration = NONE },
+    ZoFontBookSkin  = { face = 'DejaVu Serif', size = 14, decoration = NONE },
+    ZoFontBookRubbing  = { face = 'DejaVu Serif', size = 14, decoration = NONE },
+    ZoFontBookLetter  = { face = 'DejaVu Serif', size = 14, decoration = NONE },
+    ZoFontBookNote  = { face = 'DejaVu Serif', size = 14, decoration = NONE },
+    ZoFontBookScroll  = { face = 'DejaVu Serif', size = 14, decoration = NONE },
+    ZoFontBookTablet  = { face = 'DejaVu Serif', size = 14, decoration = NONE },
+    ZoFontBookPaperTitle  = { face = 'DejaVu Serif', size = 20, decoration = NONE },
+    ZoFontBookSkinTitle  = { face = 'DejaVu Serif', size = 20, decoration = NONE },
+    ZoFontBookRubbingTitle  = { face = 'DejaVu Serif', size = 20, decoration = NONE },
+    ZoFontBookLetterTitle  = { face = 'DejaVu Serif', size = 20, decoration = NONE },
+    ZoFontBookNoteTitle  = { face = 'DejaVu Serif', size = 20, decoration = NONE },
+    ZoFontBookScrollTitle  = { face = 'DejaVu Serif', size = 20, decoration = NONE },
+    ZoFontBookTabletTitle  = { face = 'DejaVu Serif', size = 20, decoration = NONE },
+    
+    ZoFontBossName  = { face = 'DejaVu Sans BoldOblique', size = 16, decoration = THIN },
+    ZoFontBoss  = { face = 'DejaVu Sans Bold', size = 12, decoration = THIN }
+}
+
+local logical = {}
+local decorations = { 'none', 'soft-shadow-thin', 'soft-shadow-thick', 'shadow' }
+
+function PawksicklesConfig:FormatFont( fontEntry )
+    local str = '%s|%d'
+    if ( fontEntry.decoration ~= NONE ) then
+        str = str .. '|%s'
+    end
+
+    return string.format( str, LMP:Fetch( LMP.MediaType.FONT, fontEntry.face ), fontEntry.size or 10, fontEntry.decoration )
+end
+
+function PawksicklesConfig:OnLoaded()
+    self.db = ZO_SavedVars:NewAccountWide( 'PAWKSICKLES_DB', 1.0, nil, defaults )
+
+    for k,_ in pairs( defaults ) do
+        tinsert( logical, k )
+    end
+
+    tsort( logical )
+
+    self.config_panel = LAM:CreateControlPanel( self._name, 'Pawksickles' )
+
+    self:BeginAddingOptions()
+end
+
+function PawksicklesConfig:BeginAddingOptions()
+    for i=1,#logical do 
+        local gameFont = tostring( logical[ i ] )
+        if ( self.db[ gameFont ] ) then 
+            CBM:FireCallbacks( 'PAWKSICKLES_FONT_CHANGED', gameFont, self:FormatFont( self.db[ gameFont ] ) )   
+
+            local header = LAM:AddHeader( self.config_panel, self._name .. '_header_' .. i, gameFont ):GetNamedChild( 'Label' )
+            header:SetFont( self:FormatFont( self.db[ gameFont ] ) )
+            self._headers[ gameFont ] = header
+
+            LAM:AddDropdown( self.config_panel, self._name .. '_font_' .. i, 'Font:', '', LMP:List( LMP.MediaType.FONT ), 
+                function() return self.db[ gameFont ].face end, 
+                function( selection )  self:FontDropdownChanged( gameFont, selection ) end )
+
+            LAM:AddSlider( self.config_panel, self._name .. '_size_' .. i, 'Size', '', 8, 50, 1, 
+                function() return self.db[ gameFont ].size end, 
+                function( size ) self:SliderChanged( gameFont, size ) end )
+
+            LAM:AddDropdown( self.config_panel, self._name .. '_decoration_' .. i, 'Decoration:', '', decorations,
+                function() return self.db[ gameFont ].decoration end,
+                function( selection ) self:DecorationDropdownChanged( gameFont, selection ) end )
+        end
+    end
+end
+
+function PawksicklesConfig:FontDropdownChanged( gameFont, fontFace )
+    self.db[ gameFont ].face = fontFace 
+    local newFont = self:FormatFont( self.db[ gameFont ] )
+    if ( self._headers[ gameFont ] ) then
+        self._headers[ gameFont ]:SetFont( newFont )
+    end
+
+    CBM:FireCallbacks( 'PAWKSICKLES_FONT_CHANGED', gameFont, newFont ) 
+end
+
+function PawksicklesConfig:SliderChanged( gameFont, size )
+    self.db[ gameFont ].size = size 
+    local newFont = self:FormatFont( self.db[ gameFont ] ) 
+    if ( self._headers[ gameFont ] ) then
+        self._headers[ gameFont ]:SetFont( newFont )
+    end
+
+    CBM:FireCallbacks( 'PAWKSICKLES_FONT_CHANGED', gameFont, newFont ) 
+end
+
+function PawksicklesConfig:DecorationDropdownChanged( gameFont, decoration )
+    self.db[ gameFont ].decoration = decoration 
+    local newFont = self:FormatFont( self.db[ gameFont ] )
+    if ( self._headers[ gameFont ] ) then
+        self._headers[ gameFont ]:SetFont( newFont )
+    end
+
+    CBM:FireCallbacks( 'PAWKSICKLES_FONT_CHANGED', gameFont, newFont ) 
+end
+
+CBM:RegisterCallback( 'PAWKSICKLES_LOADED', function( ... ) PawksicklesConfig:OnLoaded( ... ) end )
